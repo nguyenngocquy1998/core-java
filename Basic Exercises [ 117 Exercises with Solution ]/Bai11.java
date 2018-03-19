75407159ec030d1b3405ba9a452917b9be389ac0
nguyenngocquy1998 <37130781+nguyenngocquy1998@users.noreply.github.com>
2018-03-19 21:52:45 +0700

    Add files via upload

---
+++ Basic Exercises [ 117 Exercises with Solution ]/Bai11.java
@@ -0,0 +1,18 @@
+package baitap;
+
+import java.util.Scanner;
+
+public class bai11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("=======================BÀI 11=======================\n");
+		double R = 7.5;
+		double chuvi = 7.5*2*3.14;
+		double dientich = 7.5*7.5*3.14;
+		System.out.println(" chu vi = "+chuvi);
+		System.out.println(" dinh tich = "+dientich);
+	}
+
+}
